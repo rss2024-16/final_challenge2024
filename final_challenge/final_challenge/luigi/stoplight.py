a6921a5f654a8973fb40a0c5254ec320b543eee6
TrevorJohst <trejohst@mit.edu>
2024-05-09 00:15:12 +0000

    Sfafasfaf

---
+++ final_challenge/final_challenge/luigi/stoplight.py
@@ -0,0 +1,129 @@
+import rclpy
+from rclpy.node import Node
+import numpy as np
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from fc_msgs.msg import StopLightPixel
+
+from std_msgs.msg import Float32
+
+class StopLight(Node):
+    """
+    A class for applying your cone detection algorithms to the real robot.
+    Subscribes to: /zed/zed_node/rgb/image_rect_color (Image) : the live RGB image from the onboard ZED camera.
+    Publishes to: /relative_cone_px (StopLightPixel) : the coordinates of the cone in the image frame (units are pixels).
+    """
+
+    def __init__(self):
+        super().__init__("stop_light")
+
+        # Subscribe to ZED camera RGB frames
+        self.image_sub = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.image_callback, 5)
+        self.pose_sub = self.create_subscription(Odometry, self.odom_topic, self.pose_callback, 1)
+        self.debug_pub = self.create_publisher(Image, "/stoplight_debug_img", 10)
+        self.safety_pub = self.create_publisher(AckermannDriveStamped, "/vesc/low_level/input/safety", 10)
+        self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
+
+        self.stoplights = [(-10.16142463684082,16.551956176757812),
+                            (-30.105144500732422,33.97337341308594),
+                            (-54.410980224609375,22.996463775634766)]
+
+        self.ON_DISTANCE = 1.5
+        self.LOWER = 0
+        self.UPPER = 1
+
+        self.get_logger().info("Stop Light Detector Initialized")
+
+    def image_callback(self, image_msg):
+        # From your bounding box, take the center pixel on the bottom
+        # (We know this pixel corresponds to a point on the ground plane)
+        # publish this pixel (u, v) to the /relative_cone_px topic; the homography transformer will
+        # convert it to the car frame.
+
+        # Unpack the odom message
+        x = odometry_msg.pose.pose.position.x
+        y = odometry_msg.pose.pose.position.y
+
+        # Minimum distance from robot to each stoplight
+        min_dist = min(np.sqrt((x - x_i)**2 + (y - y_i)**2) for x_i, y_i in self.stoplights)
+
+        if min_dist <= self.ON_DISTANCE:
+
+            image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+
+            image_cropped = image[self.LOWER:self.UPPER,:,:]
+
+            detected, y, h = sl_color_segmentation(image_cropped, None)
+
+            if detected:
+                self.get_logger().info(f"Y value: {y}, height: {h}")
+
+                stop_cmd = AckermannDriveStamped()
+                stop_cmd.drive.speed = 0.0
+                stop_cmd.drive.steering_angle = 0.0
+                self.safety_pub.publish(stop_cmd)
+
+def sl_color_segmentation(img, template):
+	"""
+	Input:
+		img: np.3darray; the input image. BGR.
+		template_file_path; Not required, but can optionally be used to automate setting hue filter values.
+	Return:
+		detected True or False
+	"""
+    # Blur
+	#img = cv2.GaussianBlur(img, (5,5), 0)
+	#img = cv2.medianBlur(img, 5)
+	img = cv2.blur(img, (4,4))
+	img = cv2.bilateralFilter(img, 5, 75, 75)
+
+    # Convert bgr to hsv
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	RED_THRESHOLD = [
+		([0, 160, 100], [10, 255, 255]),
+        ([160, 160, 100], [180, 255, 255])
+	]
+
+    # Set lower and upper bounds for stoplight
+	lower_bound = np.array(RED_THRESHOLD[0][0])
+	upper_bound = np.array(RED_THRESHOLD[0][1])
+	lower_bound2 = np.array(RED_THRESHOLD[1][0])
+	upper_bound2 = np.array(RED_THRESHOLD[1][1])
+
+	# Mask out our colors
+	mask1 = cv2.inRange(hsv, lower_bound, upper_bound)
+	mask2 = cv2.inRange(hsv, lower_bound2, upper_bound2)
+	mask = mask1 | mask2
+
+	# Find contours from masks
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	# If we have contours
+	if len(contours) != 0:
+
+		# Find the biggest countour by area
+		c = max(contours, key = cv2.contourArea)
+
+		# Get a bounding rectangle around that contour
+		x, y, w, h = cv2.boundingRect(c)
+
+		# Add rectangle
+		cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
+
+		return True, y, h
+
+	return False, None, None
+
+def main(args=None):
+    rclpy.init(args=args)
+    cone_detector = StopLightDetector()
+    rclpy.spin(cone_detector)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- final_challenge/final_challenge/stop_detector/stop_light_detector.py
+++ final_challenge/final_challenge/stop_detector/stop_light_detector.py
@@ -23,65 +23,57 @@ class StopLightDetector(Node):
    Publishes to: /relative_cone_px (StopLightPixel) : the coordinates of the cone in the image frame (units are pixels).
    """
    def __init__(self):
-       super().__init__("stoplight_detector")
-       # toggle line follower vs cone parker
-       self.LineFollower = False
+      super().__init__("stoplight_detector")
+      # Subscribe to ZED camera RGB frames
+      self.stoplight_pub = self.create_publisher(StopLightPixel, "/relative_stoplight_px", 10)
+      self.image_sub = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.image_callback, 5)
+      self.debug_pub = self.create_publisher(Image, "/stoplight_debug_img", 10)
+      self.dist_pub = self.create_publisher(Float32,'/look_ahead',10)
+      self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
 
-       # Subscribe to ZED camera RGB frames
-       self.stoplight_pub = self.create_publisher(StopLightPixel, "/relative_stoplight_px", 10)
-       self.image_sub = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.image_callback, 5)
-       self.debug_pub = self.create_publisher(Image, "/stoplight_debug_img", 10)
-       self.dist_pub = self.create_publisher(Float32,'/look_ahead',10)
-       self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
-
-       self.get_logger().info("Stop Light Detector Initialized")
+      self.get_logger().info("Stop Light Detector Initialized")
 
    def image_callback(self, image_msg):
-       # Apply your imported color segmentation function (cd_color_segmentation) to the image msg here
-       # From your bounding box, take the center pixel on the bottom
-       # (We know this pixel corresponds to a point on the ground plane)
-       # publish this pixel (u, v) to the /relative_cone_px topic; the homography transformer will
-       # convert it to the car frame.
+      # From your bounding box, take the center pixel on the bottom
+      # (We know this pixel corresponds to a point on the ground plane)
+      # publish this pixel (u, v) to the /relative_cone_px topic; the homography transformer will
+      # convert it to the car frame.
+      
+      image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
+      #344,178.5
+      height = image.shape[1]
 
-       #################################
+      look_ahead = 2.5
+      self.dist_pub.publish(float(look_ahead))
+      v = self.get_parameter('look_ahead_v').value
 
+      if v == 0:
+         v = 168.7
 
-       image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+      # lower = int(.9*v)
+      upper = int(1.1*v)
+      if upper > height:
+         upper = height
+      # image_cropped = image[lower:upper,:,:]
+      image_copy = np.copy(image)
+      # image_copy[0:lower,:,:] = 0
+      image_copy[upper:,:,:] = 0
+      # Crop the left boundary of the image
+      image_copy[:, :upper, :] = 0
+      # image_copy[upper:, :upper, :] = 0
+      x, y, w, h, img = sl_color_segmentation(image_copy,None)
 
-       #344,178.5
-       height = image.shape[1]
+      if x is not None:
+         #send the bottom center pixel
+         center_pixel = StopLightPixel()
+         center_pixel.u = float(x + w/2)
+         center_pixel.v = float(y + h/2)
 
-       look_ahead = 2.5
-       self.dist_pub.publish(float(look_ahead))
-       v = self.get_parameter('look_ahead_v').value
+         self.stoplight_pub.publish(center_pixel)
 
-       if v == 0:
-           v = 168.7
-
-    #    lower = int(.9*v)
-       upper = int(1.1*v)
-       if upper > height:
-           upper = height
-       # image_cropped = image[lower:upper,:,:]
-       image_copy = np.copy(image)
-    #    image_copy[0:lower,:,:] = 0
-       image_copy[upper:,:,:] = 0
-        # Crop the left boundary of the image
-       image_copy[:, :upper, :] = 0
-    #    image_copy[upper:, :upper, :] = 0
-       x, y, w, h, img = sl_color_segmentation(image_copy,None)
-
-       if x is not None:
-           #send the bottom center pixel
-           center_pixel = StopLightPixel()
-           center_pixel.u = float(x + w/2)
-           center_pixel.v = float(y + h/2)
-
-           self.stoplight_pub.publish(center_pixel)
-
-           debug_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
-           self.debug_pub.publish(debug_msg)
+         debug_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+         self.debug_pub.publish(debug_msg)
 
 
 def main(args=None):
