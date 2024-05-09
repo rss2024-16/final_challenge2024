97c657dc9be0f2d1afd2a244a17aed6baa58ef0d
maxzetina <74638275+maxzetina@users.noreply.github.com>
2024-05-08 23:49:50 -0400

    Delete install directory

--- install/fc_msgs/include/fc_msgs/msg/detail/stop_sign__rosidl_typesupport_fastrtps_c.h
+++
@@ -1,1 +0,0 @@
-/home/racecar/racecar_ws/src/final_challenge2024/build/fc_msgs/rosidl_typesupport_fastrtps_c/fc_msgs/msg/detail/stop_sign__rosidl_typesupport_fastrtps_c.h
--- install/fc_msgs/include/fc_msgs/srv/lane_project.h
+++
@@ -1,1 +0,0 @@
-/home/racecar/racecar_ws/src/final_challenge2024/build/fc_msgs/rosidl_generator_c/fc_msgs/srv/lane_project.h
